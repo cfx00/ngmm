e2218eee638b15dae3347e0e520ec3567b70a6c1
empty <104560098+cfx00@users.noreply.github.com>
2023-05-13 12:05:04 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,26 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var mainNav = document.getElementById('cf-ding');
+    var mainNav = document.getElementById('biij');
     if (mainNav) {
         mainNav.innerHTML = `
-      <nav id="navbar" class="navbar navbar-expand-md navbar-light bg-transparent fixed-top">
-      <div class="container">
-          <a class="navbar-brand" href="#">春枫博客</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav ml-auto">
-                  <li class="nav-item"> <a class="nav-link" href="/">首页</a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="/biji.html">笔记</a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="/tool.html">工具页</a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="/about.html">关于</a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="/contact.html">联系</a> </li>
-            
-              </ul>
-          </div>
-      </div>
-  </nav>
+        
+        
+<div class="content">
+            <div id="ho">
+                <!-- 文章内容 -->
+                <h1>引言</h1>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+            </div>
+            <div id="js">
+                <h1>JS</h1>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+            </div>
+            <div id="css">
+                <h1>CSS</h1>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+            </div>
+            <div id="html">
+                <h1>HTML</h1>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+                <p>...</p>
+            </div>
+        </div>
       
     
       
