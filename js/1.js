59e6e2ebf6f919a1392da109843676c2d94efbf8
empty <104560098+cfx00@users.noreply.github.com>
2023-05-13 12:16:13 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -23,17 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
             <div id="ho">
                 <!-- 文章内容 -->
                 <h1>引言</h1>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
-                <p>...</p>
+                <p>这个页面是我的一些性格和我自己的一些技术过程中的记录小技巧和一些备忘的小知识希望在这个页面里面也可以找到您的想法和意见</p>    
             </div>
             <div id="js">
                 <h1>JS</h1>
+		<p>JavaScript(简称JS)是一种轻量级的脚本语言,常用于网页前端开发,实现与用户交互、动态效果和数据处理等功能。</p>
+        <h4>引用第三方库</h4>    
+        <p>可以使用 <code>&lt;script&gt;</code> 标签引用第三方的 JavaScript 库,例如:</p>
+        <pre><code>&lt;script src="https://cdn.jsdelivr.net/npm/vue"&gt;&lt;/script&gt;</code></pre>   
                 <p>...</p>
                 <p>...</p>
                 <p>...</p>
@@ -45,6 +42,16 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
             <div id="css">
                 <h1>CSS</h1>
+		<p>CSS(层叠样式表)是一种样式表语言,用于描述网页的外观和样式。</p>
+        <h4>添加样式</h4>    
+        <p>可以使用 <code>&lt;style&gt;</code> 标签或外部样式表文件添加样式,例如:</p>
+        <pre><code>&lt;style&gt;   
+          body {     
+          background-color: #f1f1f1;   
+          }  
+          &lt;/style&gt;</code></pre>
+        <p>或者:</p>
+        <pre><code>&lt;link rel="stylesheet" href="styles.css"&gt;</code></pre>           
                 <p>...</p>
                 <p>...</p>
                 <p>...</p>
