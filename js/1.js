18fd0006753a6581047ca8d84545ff387ee8ce91
empty <104560098+cfx00@users.noreply.github.com>
2023-05-13 12:08:28 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,5 +1,24 @@
+
 document.addEventListener('DOMContentLoaded', function () {
-    var mainNav = document.getElementById('biij');
+    var mainNav = document.getElementById('biij1');
+    if (mainNav) {
+        mainNav.innerHTML = `
+             <div class="sidebar d-md-block">
+            <a href="#ho" class="active">引言</a>
+            <a href="#js">JS</a>
+            <a href="#css">CSS</a>
+            <a href="#html">HTML</a>
+        </div>
+      `;
+    }
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var mainNav = document.getElementById('biij2');
     if (mainNav) {
         mainNav.innerHTML = `
         
